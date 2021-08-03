881204343cdde016dea2bbbf13adc4402104f02d
mosabami <ayobaami@hotmail.com>
2021-08-03 06:41:33 +0000

    minor tweak

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -2,4 +2,6 @@
 #!/bin/sh
 
 # add environment specific commands here
-sh -c "$(curl -fsSL  https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y
+sh -c "$(curl -fsSL  https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" 
+git config --global user.email "ayobaami@hotmail.com"
+git config --global user.name "mosabami"
