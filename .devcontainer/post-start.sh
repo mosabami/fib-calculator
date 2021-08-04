e62c0bc7a086c0db91b49cc980d22faf9003caa2
Ayobami Ayodeji <ayobaami@hotmail.com>
2021-08-04 01:01:23 +0000

    right before k8s

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -1,1 +1,5 @@
 # sed -i -r 's/ZSH_THEME=.*/ZSH_THEME="robbyrussell"/' ~/.zshrc
+git config --global user.username "mosabami"
+git config --global user.email "ayobaami@hotmail.com"
+  git config --global user.name "Ayobami Ayodeji"
+
