f5ed85207546e108ff879a24a6063007b7cf37c2
mosabami <ayobaami@hotmail.com>
2021-08-03 20:36:50 -0400

    add devcontainer

---
+++ complex/.devcontainer/post-install.sh
@@ -0,0 +1,7 @@
+  
+#!/bin/sh
+
+# add environment specific commands here
+sh -c "$(curl -fsSL  https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" 
+git config --global user.email "ayobaami@hotmail.com"
+git config --global user.name "mosabami"
